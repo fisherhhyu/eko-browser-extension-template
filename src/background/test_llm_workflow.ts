63341b8807276eb1aa54707b3b1441bb124c6be9
veasion <lzw@veasion.cn>
2025-01-02 12:00:51 +0800

    chore: modify

--- src/background/test_llm_workflow.ts
+++ src/background/test_llm_workflow.ts
@@ -1,9 +1,13 @@
 import { Eko, WorkflowParser } from "@eko-ai/eko";
-import { tools, getLLMConfig } from "@eko-ai/eko/extension";
 import { EkoConfig } from "@eko-ai/eko/types";
+import { fellou } from "@eko-ai/eko/fellou";
+import { tools, getLLMConfig } from "@eko-ai/eko/extension";
 
 export async function testWebSearchWithWorkflow() {
   let config = await getLLMConfig();
+  fellou.computer.screenshot()
+  fellou.computer.mouse_move([200, 400]);
+  fellou.computer.type("Elon Musk");
   if (!config && !config.apiKey) {
     throw Error("Please configure apiKey");
   }
@@ -15,5 +19,5 @@ export async function testWebSearchWithWorkflow() {
 
   const workflow = await eko.generateWorkflow("Search Elon Musk information and summarize it into markdown format for export");
   console.log("dsl", WorkflowParser.serialize(workflow));
-  await eko.execute(workflow);
+  await eko.executeWorkflow(workflow);
 }
